1f4d03cc10ffbb7b1a3a18cfaf3acc626a0ef275
zyt1848 <zyt1848@163.com>
2022-11-16 16:08:21 +0800

    Site updated: 2022-11-16 16:08:15

---
+++ js/menu-item-translate.js
@@ -0,0 +1,14 @@
+// add by zyt   date: 2022-11-16
+(function() {
+	var menuItemTraslate = function(item) {
+		var dict = {
+			categories: '分类',
+			tags: '标签',
+			posts: '文章',
+			about: '关于',
+			links: '友链'
+		};
+		return dict[item.toLowerCase()];
+	}
+	return menuItemTraslate;
+})();
